b8129bbf283049c6d9c04380d154c4e47ae98e3a
Earl Hans Genoso <earlhansg@gmail.com>
2018-09-24 03:00:28 +0800

    #8 Create Interface Sub-header data

---
+++ src/app/shared/interfaces/sub-header-item-model.ts
@@ -0,0 +1,54 @@
+export interface TotalUsersModel {
+    title         : string;
+    icon         ?: string;
+    href         ?: string;
+    previousTotal : number;
+    currentTotal  : number;
+  }
+
+export interface AverageTimeModel {
+    title           : string;
+    icon           ?: string;
+    href           ?: string;
+    previousAverage : number;
+    currentAverage  : number;
+  }
+
+export interface TotalMalesModel {
+    title         : string;
+    icon         ?: string;
+    href         ?: string;
+    previousTotal : number;
+    currentTotal  : number;
+  }
+
+export interface TotalFemalesModel {
+    title         : string;
+    icon         ?: string;
+    href         ?: string;
+    previousTotal : number;
+    currentTotal  : number;
+  }
+
+export interface TotalCollectionsModel {
+    title         : string;
+    icon         ?: string;
+    href         ?: string;
+    previousTotal : number;
+    currentTotal  : number;
+  }
+
+export interface TotalFemalesModel {
+    title         : string;
+    icon         ?: string;
+    href         ?: string;
+    previousTotal : number;
+    currentTotal  : number;
+  }
+export interface TotalConnectionsModel {
+    title         : string;
+    icon         ?: string;
+    href         ?: string;
+    previousTotal : number;
+    currentTotal  : number;
+  }
